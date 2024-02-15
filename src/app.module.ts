1ce0d069931a3d36e497055972e538932cf1a6af
Gauthier Auge <gauthier.auge@2dcom.fr>
2024-02-15 09:27:01 +0100

    Test d'ajout logique de pre-push + schema cars

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskService } from './tasks/task.service';
 import { TaskController } from './tasks/task.controller';
+import { CarsController } from './cars/cars.controller';
+import { CarsService } from './cars/cars.service';
+import { CarsModule } from './cars/cars.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TaskController],
-  providers: [AppService, TaskService],
+  imports: [
+    MongooseModule.forRoot('mongodb: //localhost:27017/test'),
+    CarsModule,
+  ],
+  controllers: [AppController, TaskController, CarsController],
+  providers: [AppService, TaskService, CarsService],
 })
 export class AppModule {}
